b8c9f1c922c82a71019c2b8f3de36b8e6d42672e
bgoldman22-code <bgoldman22@gmail.com>
2025-08-16 20:23:05 -0400

    Add files via upload

---
+++ src/ErrorBoundary.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div style={{padding:20, background:'yellow'}}>
+        <h2>Something went wrong.</h2>
+        <pre>{this.state.error && this.state.error.toString()}</pre>
+      </div>;
+    }
+    return this.props.children;
+  }
+}
