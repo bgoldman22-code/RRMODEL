b1a4211d8423fa613d7b1aea9ea93eae24d75202
bgoldman22-code <bgoldman22@gmail.com>
2025-08-21 10:04:44 -0400

    Add files via upload

--- src/utils/nflSchedule.js
+++ src/utils/nflSchedule.js
@@ -1,11 +1,11 @@
 import schedule from '../../data/nfl-schedule-2025.json';
 
+// Returns [1, 2, ...] from schedule entries
 export function getWeeksAvailable() {
-  // unique weeks from the schedule file
   const set = new Set(schedule.map(g => g.week));
   return Array.from(set).sort((a,b) => a - b);
 }
 
 export function getGamesForWeek(week) {
-  return schedule.filter(g => g.week === Number(week));
+  return schedule.filter(g => Number(g.week) === Number(week));
 }
