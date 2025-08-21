a23cbe274a2b088a93a27cb7c79bcd83796215ae
bgoldman22-code <bgoldman22@gmail.com>
2025-08-21 13:22:56 -0400

    Add files via upload

--- netlify/functions/nfl-rosters-run.cjs
+++ netlify/functions/nfl-rosters-run.cjs
@@ -2,8 +2,8 @@
 const { runUpdate } = require("./_shared/rosters-shared.cjs");
 
 module.exports.handler = async (event) => {
-  const qs = event?.queryStringParameters || {};
-  const debug = qs.debug === "1";
+  const params = event && event.queryStringParameters || {};
+  const debug = params.debug === "1";
   const STORE = process.env.NFL_TD_BLOBS || "nfl-td";
   const PROVIDER = process.env.NFL_ROSTERS_SOURCE || "auto";
   try{
