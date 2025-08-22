309c4a797d4af134513f348482c382ed404ac788
bgoldman22-code <bgoldman22@gmail.com>
2025-08-22 10:39:35 -0400

    Add files via upload

---
+++ app/mlb/page.jsx
@@ -0,0 +1,15 @@
+
+"use client";
+import React from "react";
+import StraightTables from "@/components/StraightTables";
+
+export default function MLBPage() {
+  const picks = (typeof window !== "undefined" && window.__PICKS__) || [];
+
+  return (
+    <div>
+      <h1>MLB Picks</h1>
+      <StraightTables picks={picks} />
+    </div>
+  );
+}
