7e09f28ef9bf0ee9c28e0506491360c8d0d70b84
bgoldman22-code <bgoldman22@gmail.com>
2025-09-11 14:16:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import MLB_HR from "./MLB_HR";
 import MLB_HITS2 from "./MLB_HITS2";
 import NFL_TD from "./NFL_TD";
-import NegCorr from "./NegCorr";
+import NFLPredictions from "./pages/NFLPredictions";
 import HRR from "./HRR";
 import HRDiagnosticsFooter from "./components/HRDiagnosticsFooter.jsx";
 
@@ -18,7 +18,7 @@ export default function App() {
           <Link to="/mlb-hits2">MLB 2+ Hits</Link>
           <Link to="/hrr">HRR</Link>
           <Link to="/nfl-td">NFL TD</Link>
-          <Link to="/nfl-negcorr">NFL NegCorr</Link>
+          <Link to="/predictions">NFLPredictions</Link>
         </div>
       </div>
       <div className="container mx-auto px-4 py-6">
@@ -28,7 +28,7 @@ export default function App() {
           <Route path="/mlb-hits2" element={<MLB_HITS2 />} />
           <Route path="/hrr" element={<HRR />} />
           <Route path="/nfl-td" element={<NFL_TD />} />
-          <Route path="/nfl-negcorr" element={<NegCorr />} />
+          <Route path="/predictions" element={<NFLPredictions />} />
         </Routes>
       </div>
     </BrowserRouter>
