bee660a61060c6dea24288427cf7e2234d8a331b
bgoldman22-code <bgoldman22@gmail.com>
2025-09-18 13:22:31 -0400

    Update nflSchedule.js

--- src/utils/nflSchedule.js
+++ src/utils/nflSchedule.js
@@ -1,5 +1,5 @@
 // src/utils/nflSchedule.js
-import schedule from '../../data/nfl-schedule-2025.json';
+import schedule from "../../public/data/nfl-schedule-2025.json";
 export function getWeeksAvailable(){
   const weeks = new Set();
   for (const g of schedule) weeks.add(g.week);
